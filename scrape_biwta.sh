e90f0fa0d11256b73286586ee0f3ea3bc2dff0d5
Jamal Khan <jamal919@gmail.com>
2024-11-25 09:56:49 +0100

    :sparkles: Updates on 202411250956

--- scrape_biwta.sh
+++ scrape_biwta.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 source /home/khan/.local/modules/miniconda/bin/activate scraper
 python scrape_biwta.py
-git commit -a -m "Updates on $(date +%Y%m%d%H%M)"
+git commit -a -m ":sparkles: Updates on $(date +%Y%m%d%H%M)"
 git push
 echo `date`
